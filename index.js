e78811124baa4d37ba4690e73bfc92cd86cdbfa6
Arsh Mann <mannarsh33@gmail.com>
2024-05-17 11:34:26 -0700

    (Added buttons to hub)

--- index.js
+++ index.js
@@ -213,6 +213,10 @@ app.get("/gameJigsawHub", (req, res) => {
   res.render("gameJigsawHub");
 });
 
+app.get("/gameSudokuHub", (req, res) => {
+  res.render("gameSudokuHub");
+});
+
 app.get("/gamesSpecific", (req, res) => {
   let gamename = req.query.game;
   gamename = gamename.charAt(0).toUpperCase() + gamename.slice(1);
