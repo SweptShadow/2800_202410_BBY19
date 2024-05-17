63236eb66f829910b79dd9e0fece0ecdaeea1e25
Dalraj Bains <dbains38@my.bcit.ca>
2024-05-17 10:01:36 -0700

    (added images on games page and linked games hub pages to games page. fixed image type issue with adobe stock images.)

--- public/js/games.js
+++ public/js/games.js
@@ -3,9 +3,13 @@ document.getElementById("game1").addEventListener('click', function() {
 });
 
 document.getElementById("game2").addEventListener('click', function() {
-    window.location.href = "gamesSpecific/?game=checkers";
+    window.location.href = "/gameCheckersHub";
 })
 
 document.getElementById("game3").addEventListener('click', function() {
-    window.location.href = "gamesSpecific/?game=jigsaw";
+    window.location.href = "/gameJigsawHub";
+})
+
+document.getElementById("game4").addEventListener('click', function() {
+    window.location.href = "/gameBingoHub";
 })
